6566614bbdb71c399eee18142176281e4386f05a
Abdulaziz Abdurasulov Abdulatif o'g'li <abdurasulov1024@gmail.com>
2024-04-22 10:56:06 +0500

    new

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -10,6 +10,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+
+        appBar: AppBar(
+           backgroundColor: Color.fromARGB(255 ,153, 17, 21),
+           title: Text('Qo\'riqlash xizmati'),
+           centerTitle: true,
+           actions: [
+
+           ],
+        ),
+
+        drawer: Drawer(
+
+          child: DrawerMenu(icon: Icons.home,text: 'Asosiy sahifa',),
+
+
+        ),
+
+
+
+
+
+
+
+        body: Container(child: Column(children: [
+
+
+        ],),),
+      ),
+    );
+  }
+}
+
+
+class DrawerMenu extends StatelessWidget {
+  const DrawerMenu({
+    super.key,
+   required this.icon,
+   required this.text,
+    
+    });
+
+final IconData icon;
+final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Column(children: [Row(children: [
+
+      Icon(icon),
+      Text(text),
+
+    ],)],),);
   }
 }
